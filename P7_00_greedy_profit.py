453a1eea45ec4dd8bec3ea9f5966739dddda5bc2
dev@KC <dev@kitconseil.fr>
2022-03-03 14:25:39 +0100

    feat: add python version of notebooks

--- P7_00_greedy_profit.py
+++ P7_00_greedy_profit.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-""" L'algorithme du sac à dos où l'on parcourt un arbre des cas possibles en décidant
-    de prendre ou non une action constituera la base de notre solution optimisée.
+""" L'algorithme glouton où l'on tri une liste dont on consomme les éléments
+    les uns après les autres jusqu'à la limite du budget.
 """
-
 # ### 1. Préparation des données
-#
-
 
 # modules importés
 # lecture de fichiers csv et dict colonnes
@@ -23,7 +19,7 @@ from sys import argv
 # constants
 FILE = "data/p7-20-shares.csv"
 FIELDNAMES = ['name', 'cost', 'profit']
-STEP = 100
+STEP = 1
 BUDGET = 500 * STEP
 
 
@@ -42,6 +38,8 @@ def fn_timer(function):
         time_end = time.perf_counter_ns()
         elapsed = (time_end-time_start)
         print(f"Total time running {function.__name__}: {str(elapsed)}s nanoseconds")
+        # elapsed = (time_end-time_start)/1000000000
+        # print(f"Total time running {function.__name__}: {str(elapsed)}s seconds")
         return result
     return function_timer
 
@@ -63,6 +61,7 @@ def clean_char(texte: str) -> str:
 
 #   lecture, nettoyage et chargement en dict.
 #     les non valeurs NaN sont rejetées.
+
 
 action_dict = {}
 file_name = FILE
@@ -120,64 +119,27 @@ def clean_data_set():
     except IOError:
         print(f" une erreur est survenue à l'écriture du fichier {file_name} : {IOError}")
 
-# ### 2. Résolution en programmation dynamique
-#
+
+# ## 2. Résolution avec un algorithme Glouton
 
 
 @fn_timer
-def measure_optimise(budget, liste):
-    """ measures run time."""
-    return knap_sack_optimise(budget, liste)
-
-
-def knap_sack_optimise(budget, actions):
-    """ buils a solution based on previous best solution."""
-    # initialize matrix of size budget x share number
-    # columns are budget increasing value whereas every line is a share
-    matrice = [[0 for x in range(0, budget + 1)] for x in range(len(actions) + 1)]
-    # for every share in book ; +1 as the zero line
-    for i in range(1, len(actions) + 1):
-        # for every budget value up to max BUDGET
-        for cout_courant in range(1, budget + 1):
-            # if enough money to buy the current share
-            if actions[i-1][1] <= cout_courant:
-                # take the max btw
-                # current share profit + the previous best solution for remaining cost
-                # btw courrent_budget - share cost
-                # the previous Max value of the same budget column
-                matrice[i][cout_courant] = max(actions[i-1][2] +
-                                               matrice[i-1][cout_courant-actions[i-1][1]],
-                                               matrice[i-1][cout_courant])
-            else:
-                # if not enough money, keep the previous solution
-                matrice[i][cout_courant] = matrice[i-1][cout_courant]
-
-    # track back the selected shares
-    budget_courant = budget
-    action_courante = len(actions)
-    actions_porte_feuille = []
-
-    while budget_courant >= 0 and action_courante >= 0:
-        action_data = actions[action_courante-1]
-        if (matrice[action_courante][budget_courant] ==
-           matrice[action_courante-1][budget_courant-action_data[1]] + action_data[2]):
-            actions_porte_feuille.append(action_data)
-            budget_courant -= action_data[1]
-
-        action_courante -= 1
-
-    return matrice[-1][-1], actions_porte_feuille
-# This code also was inspired by Algomius
+def algo_glouton(dictio: dict[str, tuple], budget: int) -> (int, list):
+    """ Tri par densité de profit càd que les actions au meilleur rapport profit/cout
+        sont les premières dans l'ordre
+    """
+    action_trie = dict(sorted(dictio.items(), key=lambda x: x[1][1], reverse=True))
+    budget_left = budget
+    action_selected: list[str] = []
+    for cle, valeur in action_trie.items():
+        if budget_left - valeur[0] > 0:
+            action_selected.append(cle)
+            budget_left -= valeur[0]
+    return (budget_left, action_selected)
 
 
 clean_data_set()
 
-liste_actions = [(cle, val[0], val[1]) for cle, val in action_dict.items()]
-valeur, action_pf = knap_sack_optimise(BUDGET, liste_actions)
-
-
-print('At cost of ', round(sum([x[1] for x in action_pf])/STEP, 2),
-      ' ', [x[0] for x in action_pf], ' actions brought a profit of ',
-      round(valeur/(STEP*STEP), 2))
-
-measure_optimise(BUDGET, liste_actions)
+reste_budget, action_pf = algo_glouton(action_dict, BUDGET)
+print('At cost of ', (BUDGET-reste_budget)/STEP, ' ', action_pf, ' actions brought a profit of ',
+      round(sum(list(map(lambda x: action_dict[x][1], action_pf)))/(STEP*STEP), 2))
